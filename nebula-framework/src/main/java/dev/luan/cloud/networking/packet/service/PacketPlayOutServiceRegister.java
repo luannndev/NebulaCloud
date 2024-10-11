28f6d1d51ef8a76f1d19a59f2a24b0467344e55b
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:49:33 +0200

    Add service management packets
    Introduce various service management packets including force shutdown, lock, register, remove, restart, and shutdown functionalities. This enables packet-based control over cloud services, enhancing the flexibility and manageability of the system.

---
+++ nebula-framework/src/main/java/dev/luan/cloud/networking/packet/service/PacketPlayOutServiceRegister.java
@@ -0,0 +1,6 @@
+package dev.luan.cloud.networking.packet.service;
+
+import dev.luan.cloud.pipe.Packet;
+
+public record PacketPlayOutServiceRegister() implements Packet {
+}
---
+++ nebula-framework/src/main/java/dev/luan/cloud/networking/packet/service/PacketPlayOutServiceRemove.java
@@ -0,0 +1,14 @@
+package dev.luan.cloud.networking.packet.service;
+
+import dev.luan.cloud.NebulaAPI;
+import dev.luan.cloud.pipe.Packet;
+import dev.luan.cloud.service.ICloudService;
+
+import java.util.Optional;
+
+public record PacketPlayOutServiceRemove(String serviceName) implements Packet {
+
+    public Optional<ICloudService> parsedCloudService() {
+        return NebulaAPI.getNebulaAPI().getProvider().getServiceProvider().getServiceByName(serviceName);
+    }
+}
