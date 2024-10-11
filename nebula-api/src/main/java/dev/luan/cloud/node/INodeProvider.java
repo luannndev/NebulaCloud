7b585e806ffd21584a158bb4bd6ddaf5bf5df057
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:22 +0200

    Add INodeProvider interface
    Introduce the INodeProvider interface to manage node retrieval operations. This interface provides methods to fetch a node by its name and retrieve a list of all nodes.

---
+++ nebula-api/src/main/java/dev/luan/cloud/node/INodeProvider.java
@@ -0,0 +1,20 @@
+package dev.luan.cloud.node;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface INodeProvider {
+
+    /**
+     * @param name String
+     * @return INode
+     */
+    Optional<INode> getNodeByName(String name);
+
+    /**
+     * Get the name of the node
+     *
+     * @return String
+     */
+    List<INode> getAllNodes();
+}
