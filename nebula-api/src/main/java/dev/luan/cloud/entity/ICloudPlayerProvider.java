faf7edf33d5198e351bfcb543235ab07d729c6fe
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:44:39 +0200

    Add ICloudPlayerProvider interface
    Introduce ICloudPlayerProvider to retrieve cloud players by username or UUID and to get a list of all online cloud players. This enhancement supports better player management within the cloud ecosystem.

---
+++ nebula-api/src/main/java/dev/luan/cloud/entity/ICloudPlayerProvider.java
@@ -0,0 +1,31 @@
+package dev.luan.cloud.entity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ICloudPlayerProvider {
+
+    /**
+     * Get a cloud player by name(String)
+     *
+     * @param username The username of the cloud player.
+     * @return ICloudPlayer This returns a cloud player by username
+     */
+    Optional<ICloudPlayer> getCloudPlayer(String username);
+
+    /**
+     * Get a cloud player by uuid(UUID)
+     *
+     * @param uniqueId The UUID of the cloud player.
+     * @return ICloudPlayer This returns a cloud player by UUID
+     */
+    Optional<ICloudPlayer> getCloudPlayer(UUID uniqueId);
+
+    /**
+     * Get all online cloud players
+     *
+     * @return List<ICloudPlayer> This returns a list of all online cloud players
+     */
+    List<ICloudPlayer> getOnlinePlayers();
+}
