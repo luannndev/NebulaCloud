6eca568de9a57dd9a9c464c08751d3e48edca2e2
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:43:29 +0200

    Create Command abstract class
    Introduce a new Command abstract class with essential attributes and methods. This includes a constructor for initialization, an abstract method for command execution, and a default tab completion method.

---
+++ nebula-api/src/main/java/dev/luan/cloud/console/command/Command.java
@@ -0,0 +1,44 @@
+package dev.luan.cloud.console.command;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+public abstract class Command implements Serializable {
+
+    private final String command;
+    private final String description;
+    private final String[] aliases;
+
+    /**
+     * Constructor to create a command.
+     *
+     * @param command     String
+     * @param description String
+     * @param aliases     String[]
+     */
+    public Command(String command, String description, String... aliases) {
+        this.command = command;
+        this.description = description;
+        this.aliases = aliases;
+    }
+
+    /**
+     * What the command should execute.
+     *
+     * @param args
+     */
+    public abstract void execute(String[] args);
+
+    /**
+     * Overwrite that methode to write your own tab completion for the command.
+     *
+     * @param args
+     * @return List<String>
+     */
+    public List<String> tabComplete(String[] args) {
+        return List.of();
+    }
+}
