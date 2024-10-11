f76866b9add69f34ae03ce48ca1a1dac918e56d2
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:39:17 +0200

    Add ServiceState and ServiceType enums to cloud service package
    Introduced ServiceState with states and associated hex colors, and ServiceType with basic service types. These additions are now part of the dev.luan.cloud.service package and implement Serializable for potential network transmission.

---
+++ nebula-api/src/main/java/dev/luan/cloud/service/ServiceState.java
@@ -0,0 +1,20 @@
+package dev.luan.cloud.service;
+
+import java.io.Serializable;
+
+public enum ServiceState implements Serializable {
+    PREPARING("#d1a879"),
+    STARTING(""),
+    ONLINE("#91d177"),
+    INVISIBLE("#5c5c5c");
+
+    private final String hex;
+
+    ServiceState(String hex) {
+        this.hex = hex;
+    }
+
+    public String getHex() {
+        return hex;
+    }
+}
---
+++ nebula-api/src/main/java/dev/luan/cloud/service/ServiceType.java
@@ -0,0 +1,9 @@
+package dev.luan.cloud.service;
+
+import java.io.Serializable;
+
+public enum ServiceType implements Serializable {
+    Lobby,
+    Proxy,
+    Server;
+}
