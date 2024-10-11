c3ea8b9527db6f3cf561f484b95687157c96b311
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:55 +0200

    Add Listener interface to handle event listening
    Introduced a Listener interface with a default isActive method. This method can be overridden to determine if a listener is active, offering flexibility for runtime toggling.

---
+++ nebula-api/src/main/java/dev/luan/cloud/event/Listener.java
@@ -0,0 +1,14 @@
+package dev.luan.cloud.event;
+
+public interface Listener {
+
+    /**
+     * Override this with your own logic e.g. if you want to toggle listeners during runtime.
+     *
+     * @return Boolean when the listener is active true else false
+     */
+    default boolean isActive() {
+        return true;
+    }
+
+}
