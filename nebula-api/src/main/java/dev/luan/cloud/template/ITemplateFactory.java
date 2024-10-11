9d06c886640d5cd9d4fc4e7401de886d83989ca6
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:37 +0200

    Add ITemplateFactory interface for template management
    Introduced ITemplateFactory interface to handle creation and deletion of templates by name. This addition standardizes template management within the project.

---
+++ nebula-api/src/main/java/dev/luan/cloud/template/ITemplateFactory.java
@@ -0,0 +1,18 @@
+package dev.luan.cloud.template;
+
+public interface ITemplateFactory {
+
+    /**
+     * Create a template with defined name
+     *
+     * @param name String
+     */
+    ITemplate createTemplate(String name);
+
+    /**
+     * Delete a template by name
+     *
+     * @param name
+     */
+    void deleteTemplate(String name);
+}
