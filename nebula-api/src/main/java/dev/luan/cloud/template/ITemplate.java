d8ac5fbad7b86f29e1e08b680763c9e35c3f051c
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:30 +0200

    Add ITemplate interface to define template structure
    Introduce `ITemplate` interface with `getName` and `getPath` methods to establish a consistent structure for templates. This change is essential for providing a reusable and standardized way to manage template names and paths, facilitating better organization and interaction within the system.

---
+++ nebula-api/src/main/java/dev/luan/cloud/template/ITemplate.java
@@ -0,0 +1,21 @@
+package dev.luan.cloud.template;
+
+import java.io.Serializable;
+import java.nio.file.Path;
+
+public interface ITemplate extends Serializable {
+
+    /**
+     * Get the name of the template
+     *
+     * @return String
+     */
+    String getName();
+
+    /**
+     * Get the path of the template
+     *
+     * @return Path
+     */
+    Path getPath();
+}
