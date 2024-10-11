c02b6e314652206d692deb785cc7b854fb575e02
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:47 +0200

    Add ITemplateProvider interface for template management
    Introduce ITemplateProvider with methods to retrieve templates by name and to list all available templates. This facilitates access to template data within the application.

---
+++ nebula-api/src/main/java/dev/luan/cloud/template/ITemplateProvider.java
@@ -0,0 +1,22 @@
+package dev.luan.cloud.template;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITemplateProvider {
+
+    /**
+     * Get template by name
+     *
+     * @param name
+     * @return ITemplate
+     */
+    Optional<ITemplate> getTemplateByName(String name);
+
+    /**
+     * Returns a list of all exist templates
+     *
+     * @return List<ITemplate>
+     */
+    List<ITemplate> getAllTemplates();
+}
