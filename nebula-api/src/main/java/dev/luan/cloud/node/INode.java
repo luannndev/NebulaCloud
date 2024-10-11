44594e2319e8b38444280b7554acdab21b7fe7b4
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:14 +0200

    Add INode interface for node management in the cloud
    This commit introduces the INode interface, defining methods for retrieving node information such as name, address, and memory usage. It also includes methods for updating node properties and checking if the node is connected to the current node. This interface standardizes interactions with individual nodes in the system.

---
+++ nebula-api/src/main/java/dev/luan/cloud/node/INode.java
@@ -0,0 +1,61 @@
+package dev.luan.cloud.node;
+
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+
+public interface INode extends Serializable {
+
+    /**
+     * Get the name of the node
+     *
+     * @return String
+     */
+    String getName();
+
+    /**
+     * Get the address of the node
+     *
+     * @return InetSocketAddress
+     */
+    InetSocketAddress getAddress();
+
+    /**
+     * Get if the node is connected with the current node.
+     *
+     * @return Boolean This returns if the node is connected. (true if yes else false)
+     */
+    boolean isConnected();
+
+    /**
+     * Get the used memory of the node.
+     *
+     * @return Long This returns the used memory of the node.
+     */
+    long getUsedMemory();
+
+    /**
+     * Set the used memory of the node.
+     *
+     * @param usedMemory
+     */
+    void setUsedMemory(long usedMemory);
+
+    /**
+     * Get the max memory of the node.
+     *
+     * @return Long This returns the max memory of the node.
+     */
+    long getMaxMemory();
+
+    /**
+     * Set the max memory of the node.
+     *
+     * @param maxMemory
+     */
+    void setMaxMemory(long maxMemory);
+
+    /**
+     * Update the node.
+     */
+    void update();
+}
