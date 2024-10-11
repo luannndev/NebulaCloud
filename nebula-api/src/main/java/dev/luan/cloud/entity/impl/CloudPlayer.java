6c7cacae36db6f6d4af7d1745ef5fc1fec051f7f
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:41:54 +0200

    Add CloudPlayer implementation
    Introduced a new CloudPlayer class implementing ICloudPlayer. This class includes fields for username, uniqueId, address, skin data, and connected service, along with corresponding getters and update methods.

---
+++ nebula-api/src/main/java/dev/luan/cloud/entity/impl/CloudPlayer.java
@@ -0,0 +1,71 @@
+package dev.luan.cloud.entity.impl;
+
+import dev.luan.cloud.entity.ICloudPlayer;
+import dev.luan.cloud.service.ICloudService;
+import lombok.AllArgsConstructor;
+
+import java.net.InetSocketAddress;
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class CloudPlayer implements ICloudPlayer {
+
+    private String username;
+    private UUID uniqueId;
+    private InetSocketAddress address;
+    private String skinValue;
+    private String skinSignature;
+    private Optional<ICloudService> connectedService;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public UUID getUniqueId() {
+        return uniqueId;
+    }
+
+    @Override
+    public InetSocketAddress getAddress() {
+        return address;
+    }
+
+    @Override
+    public String getSkinValue() {
+        return skinValue;
+    }
+
+    @Override
+    public String getSkinSignature() {
+        return skinSignature;
+    }
+
+    @Override
+    public Optional<ICloudService> getConnectedCloudService() {
+        return connectedService;
+    }
+
+    @Override
+    public void updateUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public void updateAddress(InetSocketAddress address) {
+        this.address = address;
+    }
+
+    @Override
+    public void updateConnectedService(ICloudService cloudService) {
+        this.connectedService = Optional.of(cloudService);
+    }
+
+    @Override
+    public void updateSkinData(String skinValue, String skinSignature) {
+        this.skinValue = skinValue;
+        this.skinSignature = skinSignature;
+    }
+}
