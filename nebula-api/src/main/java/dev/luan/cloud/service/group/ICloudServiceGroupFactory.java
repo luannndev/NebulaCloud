2c4200c21aa7e3c079560edb7fb23aa251928235
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:45:11 +0200

    Add ICloudServiceGroupFactory interface
    Introduce the ICloudServiceGroupFactory interface to handle the creation and deletion of cloud service groups. This includes methods for creating lobby, proxy, and server groups, streamlining the management of various service group types.

---
+++ nebula-api/src/main/java/dev/luan/cloud/service/group/ICloudServiceGroupFactory.java
@@ -0,0 +1,73 @@
+package dev.luan.cloud.service.group;
+
+import dev.luan.cloud.node.INode;
+import dev.luan.cloud.service.group.impl.DefaultLobbyGroup;
+import dev.luan.cloud.service.group.impl.DefaultProxyGroup;
+import dev.luan.cloud.service.group.impl.DefaultServerGroup;
+import dev.luan.cloud.template.ITemplate;
+
+import java.util.List;
+
+public interface ICloudServiceGroupFactory {
+
+    /**
+     * Use this methode to create a lobby service group
+     *
+     * @param name            String
+     * @param groupTitle      String
+     * @param node            INode
+     * @param templates       List<ITemplate>
+     * @param version         String
+     * @param maximumPlayers  Int
+     * @param memory          Int
+     * @param minimalServices Int
+     * @param maximalServices Int
+     * @return ICloudServiceGroup
+     */
+    default ICloudServiceGroup createLobbyGroup(String name, String groupTitle, INode node, List<ITemplate> templates, String version, boolean maintenance, boolean isStatic, int maximumPlayers, int memory, int minimalServices, int maximalServices) {
+        return new DefaultLobbyGroup(name, groupTitle, node, templates, version, maintenance, isStatic, maximumPlayers, memory, minimalServices, maximalServices).initFile();
+    }
+
+    /**
+     * Use this methode to create a proxy service group
+     *
+     * @param name            String
+     * @param groupTitle      String
+     * @param node            INode
+     * @param templates       List<ITemplate>
+     * @param version         String
+     * @param maximumPlayers  Int
+     * @param memory          Int
+     * @param minimalServices Int
+     * @param maximalServices Int
+     * @return ICloudServiceGroup
+     */
+    default ICloudServiceGroup createProxyGroup(String name, String groupTitle, INode node, List<ITemplate> templates, String version, boolean maintenance, boolean isStatic, int port, int maximumPlayers, int memory, int minimalServices, int maximalServices) {
+        return new DefaultProxyGroup(name, groupTitle, node, templates, version, maintenance, isStatic, port, maximumPlayers, memory, minimalServices, maximalServices).initFile();
+    }
+
+    /**
+     * Use this methode to create a server service group
+     *
+     * @param name            String
+     * @param groupTitle      String
+     * @param node            INode
+     * @param templates       List<ITemplate>
+     * @param version         String
+     * @param maximumPlayers  Int
+     * @param memory          Int
+     * @param minimalServices Int
+     * @param maximalServices Int
+     * @return ICloudServiceGroup
+     */
+    default ICloudServiceGroup createServerGroup(String name, String groupTitle, INode node, List<ITemplate> templates, String version, boolean maintenance, boolean isStatic, int maximumPlayers, int memory, int minimalServices, int maximalServices) {
+        return new DefaultServerGroup(name, groupTitle, node, templates, version, maintenance, isStatic, maximumPlayers, memory, minimalServices, maximalServices).initFile();
+    }
+
+    /**
+     * Use this methode to delete a service group
+     *
+     * @param iCloudServiceGroup ICloudServiceGroup
+     */
+    void deleteServiceGroup(ICloudServiceGroup iCloudServiceGroup);
+}
