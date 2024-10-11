68cc1b540eb090e17a4999703ce24a1bd828c7c0
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:43:57 +0200

    Add abstract Event class to nebula-api
    Introduced the `Event` class in the package `dev.luan.cloud.event`. This class must be extended by every event created and implements `Serializable` for potential use in distributed systems.

---
+++ nebula-api/src/main/java/dev/luan/cloud/event/Event.java
@@ -0,0 +1,10 @@
+package dev.luan.cloud.event;
+
+import java.io.Serializable;
+
+/**
+ * This class is for important for every event you create.
+ * You need to extend from that class!
+ */
+public abstract class Event implements Serializable {
+}
