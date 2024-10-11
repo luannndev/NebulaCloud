d94d185979e8c8131b1ae3252a21e400499a6fc2
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:46:07 +0200

    Add IModuleProvider interface
    Introduced IModuleProvider to handle module loading and retrieval. This includes methods to load a module by path, get a module by name, and retrieve all loaded modules.

---
+++ nebula-api/src/main/java/dev/luan/cloud/module/IModuleProvider.java
@@ -0,0 +1,29 @@
+package dev.luan.cloud.module;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IModuleProvider {
+
+    /**
+     * load a module by path
+     *
+     * @param path
+     */
+    void loadModule(String path);
+
+    /**
+     * Get a module by name
+     *
+     * @param name
+     * @return ILoadedModule This returns the module by name.
+     */
+    Optional<ILoadedModule> getModuleByName(String name);
+
+    /**
+     * Get all loaded modules
+     *
+     * @return List<ILoadedModule> This returns a list of all loaded modules.
+     */
+    List<ILoadedModule> getAllModules();
+}
