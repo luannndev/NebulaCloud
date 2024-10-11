43997c55c46c6008bbd3c816df61745ca621e146
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:45:42 +0200

    Add IEventProvider interface to handle event mechanisms
    Introduces the IEventProvider interface which includes methods for calling custom events and subscribing listeners. This will standardize the way events are handled and listeners are managed within the cloud module.

---
+++ nebula-api/src/main/java/dev/luan/cloud/event/IEventProvider.java
@@ -0,0 +1,19 @@
+package dev.luan.cloud.event;
+
+public interface IEventProvider {
+
+    /**
+     * Use that methode to call a custom event.
+     *
+     * @param event Event
+     * @param <T>   T
+     */
+    <T extends Event> void callEvent(T event);
+
+    /**
+     * Use this methode to subscribe a cloud listener.
+     *
+     * @param listener Listener
+     */
+    void subscribeListener(Listener listener);
+}
