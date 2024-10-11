6c33889c033354289f77673b2c0ec27d3715a87b
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-11 12:48:03 +0200

    Add PacketPlayOutServiceStart implementation
    Introduce PacketPlayOutServiceStart to handle service start packets. This class includes methods for parsing the cloud service and node by their names using the NebulaAPI.

---
+++ nebula-framework/src/main/java/dev/luan/cloud/networking/packet/service/PacketPlayOutServiceStart.java
@@ -0,0 +1,19 @@
+package dev.luan.cloud.networking.packet.service;
+
+import dev.luan.cloud.NebulaAPI;
+import dev.luan.cloud.node.INode;
+import dev.luan.cloud.pipe.Packet;
+import dev.luan.cloud.service.ICloudService;
+
+import java.util.Optional;
+
+public record PacketPlayOutServiceStart(String serviceName, String node) implements Packet {
+
+    public Optional<ICloudService> parsedCloudService() {
+        return NebulaAPI.getNebulaAPI().getProvider().getServiceProvider().getServiceByName(serviceName);
+    }
+
+    public Optional<INode> parsedNode() {
+        return NebulaAPI.getNebulaAPI().getProvider().getNodeProvider().getNodeByName(node);
+    }
+}
